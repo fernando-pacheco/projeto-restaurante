a0ed5f20bfda1ff1b79ff6782ce55d2af18f0311
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-11-22 08:35:18 -0300

    [b/m] chore: tornando dsn variável de ambiente

--- management/src/db.py
+++ management/src/db.py
@@ -1,16 +1,17 @@
+import os
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import SQLAlchemyError
 
-import os
-
 db_instance = SQLAlchemy()
 
-POSTGRES_DSN = f'postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@db:5432/{os.getenv('DEFAULT_DB_NAME')}'
 
 def config_sql_alchemy(app):
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = POSTGRES_DSN
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('POSTGRES_DSN')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv(
+        'SQLALCHEMY_TRACK_MODIFICATIONS'
+    )
     app.config['SQLALCHEMY_ECHO'] = os.getenv('SQLALCHEMY_ECHO')
     db_instance.app = app
     db_instance.init_app(app)
@@ -25,4 +26,5 @@ def db_persist(func):
         except SQLAlchemyError:
             db_instance.session.rollback()
             return False
+
     return persist
