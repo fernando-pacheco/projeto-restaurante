0fbb264bcf4c221308d4a46c2e363c5f6dc04828
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-11-25 17:19:14 -0300

    [management] feat: schema para CRUD de função funcionário

---
+++ management/src/schemas/funcao_funcionario.py
@@ -0,0 +1,30 @@
+from src.schema import ma
+from marshmallow import Schema, fields
+
+
+class FuncaoFuncionarioResponseSchema(ma.Schema):
+    id = fields.Int()
+    funcionario_id = fields.UUID()
+    funcao_id = fields.Int()
+
+    class Meta:
+        fields = ('id', 'funcionario_id', 'funcao_id')
+
+    _links = ma.Hyperlinks({'self': ma.URLFor('funcao-funcionario')})
+
+
+class FuncaoFuncionarioResquestGetSchema(Schema):
+    id = fields.Int(required=True)
+
+
+class FuncaoFuncionarioResquestPostSchema(Schema):
+    funcionario_id = fields.UUID(required=True)
+    funcao_id = fields.Int(required=True)
+
+
+class FuncaoFuncionarioResquestPutSchema(Schema):
+    funcionario_id = fields.UUID()
+    funcao_id = fields.Int()
+
+
+funcao_funcionario_schema = FuncaoFuncionarioResponseSchema()
