e3d66959fb78a81fe7d5185736f0e59b79872b18
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-11-25 16:29:45 -0300

    [management] chore: configuração dos meta dados swagger

--- management/src/swagger_docs.py
+++ management/src/swagger_docs.py
@@ -5,10 +5,13 @@ from flask_apispec.extension import FlaskApiSpec
 
 def config_swagger(app):
     spec = APISpec(
-        title='Sample Project - APIs Documentation',
+        title='Gerenciador de Usuários - Documentação API',
         version='v1',
         plugins=[MarshmallowPlugin()],
         openapi_version='2.0.0',
+        info=dict(
+            description='API para gerenciamento de clientes e colaboradores do projeto Restaurantes'
+        ),
     )
 
     app.config.update(
