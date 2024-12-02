2dafd02193e01af0a562fd8f155bf1f6da1d7150
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-02 15:21:35 -0300

    [frontend] chore: configuração da aplicação front de js para ts

---
+++ frontend/src/app.tsx
@@ -0,0 +1,24 @@
+export function App() {
+  return (
+    <div className="px-12 h-full flex flex-col">
+      <header className="flex justify-between py-6">
+        <div className="flex space-x-6">
+          <div>Logo</div>
+          <div>Restaurantes</div>
+          <div>Seja Parceiro</div>
+          <div>Fale conosco</div>
+        </div>
+        <div className="flex space-x-6">
+          <div>Registre-se</div>
+          <div>Login</div>
+        </div>
+      </header>
+      <body className="flex space-x-6 py-6">
+        Body
+      </body>
+      <footer className="flex py-6">
+        Footer
+      </footer>
+    </div>
+  )
+}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,16 @@
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}",
-  ],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
-    extend: {},
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+}
