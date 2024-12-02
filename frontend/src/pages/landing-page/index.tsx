569617265fe0dbedba66c3929a82ba48b944e3c4
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-02 16:15:59 -0300

    [frontend] feat: configuração das rotas login e principal

---
+++ frontend/src/pages/landing-page/index.tsx
@@ -0,0 +1,25 @@
+export function LandingPage() {
+    return (
+        <div className="h-screen flex flex-col">
+            <header className="flex justify-between py-6">
+                <div className="flex space-x-6">
+                    <div>Logo</div>
+                    <div>Restaurantes</div>
+                    <div>Seja Parceiro</div>
+                    <div>Fale conosco</div>
+                </div>
+                <div className="flex space-x-6">
+                    <div>Registre-se</div>
+                    <div>Login</div>
+                </div>
+            </header>
+            <body className="flex flex-col space-x-6 space-y-6 py-6 flex-1 items-center justify-center">
+                Página principal - Landing Page
+                <img src="static/bg-logo.png" alt="LOGO" width={300} height={300} />
+            </body>
+            <footer className="flex py-6 justify-center">
+                Footer
+            </footer>
+        </div>
+    )
+}
