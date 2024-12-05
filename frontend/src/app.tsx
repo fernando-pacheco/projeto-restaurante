3d3dea92aad63ac777c0ff5a3b11374bb5840ad6
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-05 16:01:57 -0300

    [frontend] feat: exibição de toast para toda aplicação

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -2,10 +2,12 @@ import { Routes, Route } from "react-router-dom"
 import { Login } from "./pages/login"
 import { LandingPage } from "./pages/landing-page"
 import { Home } from "./pages/home"
+import { Toaster } from "sonner"
 
 export function App() {
   return (
     <div>
+      <Toaster />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
