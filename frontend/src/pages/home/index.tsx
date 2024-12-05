362b346cf1f7c80982ff1321e0bf3c9b4171a76c
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-05 16:05:28 -0300

    [frontend] feat: página inicial pós autenticação

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,12 +1,26 @@
 import { Button } from "@/components/molecules/button";
+import { useState } from "react";
+import { InfosUsuarioProps } from "@/service/interface/infos-usuario-interface";
+import usuarioInfoService from "@/service/usuario-info";
 
 export function Home() {
+    const [infosUsuario, setInfosUsuario] = useState<InfosUsuarioProps | null>(null)
+
+    async function obterInfosUsuario() {
+        const response = await usuarioInfoService.obterInfoUsuario()
+        if (response && response.data) {
+            setInfosUsuario(response.data)
+        } else {
+            console.error("Nenhum dado retornado.")
+        }
+    }
 
     return (
-        <div>
+        <div className="space-y-4">
             <h1>Página principal da aplicação</h1>
             <Button
-                className="bg-sky-800 hover:"
+                onClick={obterInfosUsuario}
+                variant={"primary"}
             >
                 Obter informações do usuário
             </Button>
