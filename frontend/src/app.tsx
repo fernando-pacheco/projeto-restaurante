6cf6627aa4f9dcfc7b47c4f606a08c0d03adb651
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-05 16:35:47 -0300

    [frontend] feat: rota de redirecionamento para registro

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -3,6 +3,7 @@ import { Login } from "./pages/login"
 import { LandingPage } from "./pages/landing-page"
 import { Home } from "./pages/home"
 import { Toaster } from "sonner"
+import { RegisterCliente } from "./pages/register/cliente"
 
 export function App() {
   return (
@@ -12,6 +13,7 @@ export function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/register" element={<RegisterCliente />} />
       </Routes>
     </div>
   )
