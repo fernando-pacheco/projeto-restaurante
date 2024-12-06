2677bf3b58175107526f8f91d863b7a68b08c95d
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-06 11:40:20 -0300

    [frontend] feat: definindo layout base para as páginas de autenticação

---
+++ frontend/src/components/organisms/auth-base.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from "react"
+
+interface AuthBaseProps {
+    children: ReactNode
+}
+
+export function AuthBase({ children }: AuthBaseProps) {
+    return (
+        <div className="relative flex items-center min-h-screen w-screen bg-gradient-to-b from-salmon-50 to bg-salmon-700/15 pl-12 overflow-hidden">
+            <div className="absolute inset-0 -z-10">
+                <circle className="border-4 border-salmon-900/10 w-[800px] h-[800px] rounded-full absolute bottom-1/2 -left-16 z-0" />
+                <circle className="border-2 border-salmon-950/10 w-[800px] h-[800px] rounded-full absolute bottom-1/2 -left-16 z-0" />
+                <circle className="border-4 border-salmon-900/10 w-[800px] h-[800px] rounded-full absolute top-36 -left-24 z-0" />
+                <circle className="border-2 border-salmon-950/10 w-[800px] h-[800px] rounded-full absolute top-36 -left-24 z-0" />
+                <circle className="border-4 border-salmon-900/10 w-[800px] h-[800px] rounded-full absolute top-40 -left-16 z-0" />
+                <circle className="border-2 border-salmon-950/10 w-[800px] h-[800px] rounded-full absolute top-40 -left-16 z-0" />
+                <circle className="bg-gradient-to-b from-salmon-700/5 to-salmon-700/15 w-[1000px] h-[1000px] rounded-full absolute bottom-16 -right-40" />
+                <circle className="bg-gradient-to-t from-salmon-700/5 to-salmon-700/5 w-[2000px] h-[2000px] rounded-full absolute top-96 left-4" />
+            </div>
+            {children}
+        </div>
+    )
+}
