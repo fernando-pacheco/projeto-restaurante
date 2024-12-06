9154f773b279e283e1b757bfe0226a8e2da5fab7
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-06 16:46:15 -0300

    [frontend] refactor: ordem na hierarquia das tags

--- frontend/src/components/organisms/auth-base.tsx
+++ frontend/src/components/organisms/auth-base.tsx
@@ -7,9 +7,10 @@ interface AuthBaseProps {
 export function AuthBase({ children }: AuthBaseProps) {
     return (
         <div className="relative flex items-center min-h-screen w-screen bg-gradient-to-b from-salmon-50 to bg-salmon-700/15 pl-12 overflow-hidden">
+            {children}
             <div className="absolute inset-0 -z-10">
-                <circle className="border-4 border-salmon-900/10 w-[800px] h-[800px] rounded-full absolute bottom-1/2 -left-16 z-0" />
-                <circle className="border-2 border-salmon-950/10 w-[800px] h-[800px] rounded-full absolute bottom-1/2 -left-16 z-0" />
+                <circle className="border-4 border-salmon-900/10 w-[800px] h-[800px] rounded-full absolute bottom-1/2 left-10 z-0" />
+                <circle className="border-2 border-salmon-950/10 w-[800px] h-[800px] rounded-full absolute bottom-1/2 left-10 z-0" />
                 <circle className="border-4 border-salmon-900/10 w-[800px] h-[800px] rounded-full absolute top-36 -left-24 z-0" />
                 <circle className="border-2 border-salmon-950/10 w-[800px] h-[800px] rounded-full absolute top-36 -left-24 z-0" />
                 <circle className="border-4 border-salmon-900/10 w-[800px] h-[800px] rounded-full absolute top-40 -left-16 z-0" />
@@ -17,7 +18,6 @@ export function AuthBase({ children }: AuthBaseProps) {
                 <circle className="bg-gradient-to-b from-salmon-700/5 to-salmon-700/15 w-[1000px] h-[1000px] rounded-full absolute bottom-16 -right-40" />
                 <circle className="bg-gradient-to-t from-salmon-700/5 to-salmon-700/5 w-[2000px] h-[2000px] rounded-full absolute top-96 left-4" />
             </div>
-            {children}
             <div className="flex flex-1 justify-center h-screen w-full items-center">
                 <img
                     alt="img-fundo"
