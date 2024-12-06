6faae9a8ab17f85df16125cb790b5e25fae075f1
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-06 17:58:48 -0300

    [frontend] feat: página de confimação de alteração de senha

---
+++ frontend/src/pages/auth/confirmed-password/index.tsx
@@ -0,0 +1,9 @@
+import { AuthBase } from "@/components/organisms/auth-base"
+
+export function ConfirmedPassword() {
+    return (
+        <AuthBase>
+            <>Senha alterada com sucesso!</>
+        </AuthBase>
+    )
+}
