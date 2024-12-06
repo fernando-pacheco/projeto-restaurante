8245a87ba67f262499bffb3a57d6076f552a2bd2
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-06 17:55:36 -0300

    [frontend] refactor: padrão de cores para o input

--- frontend/src/components/molecules/input.tsx
+++ frontend/src/components/molecules/input.tsx
@@ -3,19 +3,19 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
-  ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-10 w-full rounded-md border border-zinc-900 bg-white px-3 py-2 text-base ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-zinc-950 placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm dark:border-zinc-800 dark:bg-zinc-950 dark:ring-offset-zinc-950 dark:file:text-zinc-50 dark:placeholder:text-zinc-400 dark:focus-visible:ring-zinc-300",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
+    ({ className, type, ...props }, ref) => {
+        return (
+            <input
+                type={type}
+                className={cn(
+                    "flex h-10 w-full rounded-md border border-salmon-700 bg-white px-3 py-2 text-base ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-salmon-800 placeholder:text-salmon-900/50 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-salmon-800 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm dark:border-salmon-800 dark:bg-salmon-950 dark:ring-offset-salmon-950 dark:file:text-salmon-50 dark:placeholder:text-salmon-400 dark:focus-visible:ring-salmon-300",
+                    className
+                )}
+                ref={ref}
+                {...props}
+            />
+        )
+    }
 )
 Input.displayName = "Input"
 
