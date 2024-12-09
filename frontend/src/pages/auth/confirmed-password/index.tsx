7f3356e383d34e59233e8b91fe6bc9bcb13961b6
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-09 15:03:59 -0300

    [frontend] cleanup: remoção de componente

--- frontend/src/pages/auth/confirmed-password/index.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthBase } from "@/components/organisms/auth-base"
-
-export function ConfirmedPassword() {
-    return (
-        <AuthBase>
-            <>Senha alterada com sucesso!</>
-        </AuthBase>
-    )
-}
