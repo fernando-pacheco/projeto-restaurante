391b882f66a04cdeedbe3e791af0f27dde924c14
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-11 09:12:23 -0300

    [frontend] feat: layout página inicial - aplicação de landing page

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -3,27 +3,24 @@ import { LayoutHome } from "@/components/organisms/layout-home"
 export function Home() {
     return (
         <LayoutHome>
-            <div className="flex flex-col h-screen">
-                <div className="flex space-x-4">
-                    <div className="flex justify-center items-center w-1/2 h-1/2 bg-zinc-200 rounded-lg">
-                        layer1.1
-                    </div>
-                    <div className="flex justify-center items-center w-1/2 h-1/2 bg-zinc-200 rounded-lg">
-                        layer1.2
-                    </div>
+            <div className="grid grid-cols-4 gap-4 h-[1000px]">
+                <div className="flex justify-center col-span-2 items-center bg-zinc-200 rounded-lg">
+                    layer1.1
                 </div>
-                <div className="flex space-x-2">
-                    <div className="flex justify-center items-center w-1/3 h-1/2 bg-zinc-200 rounded-lg">
-                        layer2.1
-                    </div>
-                    <div className="flex justify-center items-center w-1/3 h-1/2 bg-zinc-200 rounded-lg">
-                        layer2.2
-                    </div>
-                    <div className="flex justify-center items-center w-1/3 h-1/2 bg-zinc-200 rounded-lg">
-                        layer2.3
-                    </div>
+                <div className="flex justify-center col-span-2 items-center bg-zinc-200 rounded-lg">
+                    layer1.2
+                </div>
+                <div className="flex justify-center items-center bg-zinc-200 rounded-lg">
+                    layer2.1
+                </div>
+                <div className="flex justify-center col-span-2 items-center bg-zinc-200 rounded-lg">
+                    layer2.2
+                </div>
+                <div className="flex justify-center items-center bg-zinc-200 rounded-lg">
+                    layer2.3
                 </div>
             </div>
+            <div className="w-full h-[1000px] bg-black"></div>
         </LayoutHome>
     )
 }
