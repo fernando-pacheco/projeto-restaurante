f20c96f75fa2494753ff3b6212295b44f39efc70
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-11 08:53:52 -0300

    [frontend] cleanup: remoção de importação e funcionalidade não utilizada

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,23 +1,6 @@
-import { useState } from "react"
-import { InfosUsuarioProps } from "@/service/interface/infos-usuario-interface"
-import { UsuarioInfoService } from "@/service/usuario-info"
 import { LayoutHome } from "@/components/organisms/layout-home"
 
 export function Home() {
-    const [infosUsuario, setInfosUsuario] = useState<InfosUsuarioProps | null>(
-        null,
-    )
-
-    async function obterInfosUsuario() {
-        const service = new UsuarioInfoService()
-        const response = await service.getInfoUsuario()
-        if (response && response.data) {
-            setInfosUsuario(response.data)
-        } else {
-            console.error("Nenhum dado retornado.")
-        }
-    }
-
     return (
         <LayoutHome>
             <div className="flex flex-col h-screen">
