74f6a2b81f060bd15d24d1b82827325a0ca0c34d
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-11 16:15:59 -0300

    [frontend] feat: padronização de layout e cores do leyout-home

--- frontend/src/components/organisms/layout-home.tsx
+++ frontend/src/components/organisms/layout-home.tsx
@@ -10,10 +10,10 @@ export function LayoutHome({ children }: { children: ReactNode }) {
     return (
         <SidebarProvider>
             <AppSidebar />
-            <SidebarInset>
-                <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+            <SidebarInset className="bg-zinc-5050">
+                <header className="flex h-12 shrink-0 items-center transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
                     <div className="flex items-center gap-2 px-4">
-                        <SidebarTrigger className="-ml-1" />
+                        <SidebarTrigger />
                     </div>
                 </header>
                 <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
