6da87f53bfdf1bc4489302e8ea3739d999aec524
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-12 15:29:56 -0300

    [frontend] feat: componente de card de busca

---
+++ frontend/src/components/molecules/card-search.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react"
+import { Card, CardContent, CardDescription } from "../atoms/card"
+
+interface CardSearchProp {
+    icon: ReactNode
+    description: string
+}
+
+export function CardSearch({ description, icon }: CardSearchProp) {
+    return (
+        <Card className="bg-transparent">
+            <button>
+                <CardContent className="flex flex-col justify-between items-center flex-1 w-36 h-28 p-5">
+                    {icon}
+                    <CardDescription>{description}</CardDescription>
+                </CardContent>
+            </button>
+        </Card>
+    )
+}
