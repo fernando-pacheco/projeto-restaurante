cbd6dbab3903994812f811b0cafbea53682fbae0
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-12 15:31:59 -0300

    [frontend] feat: mock do conteúdo da barra de pesquisa

---
+++ frontend/src/utils/label-search.ts
@@ -0,0 +1,44 @@
+import { ChefHat, CircleStop, HardHat, SquareMenu } from "lucide-react"
+
+export const dataSearch = [
+    {
+        icon: SquareMenu,
+        description: "Categoria 1",
+    },
+    {
+        icon: CircleStop,
+        description: "Categoria 2",
+    },
+    {
+        icon: HardHat,
+        description: "Categoria 3",
+    },
+    {
+        icon: ChefHat,
+        description: "Categoria 4",
+    },
+    {
+        icon: CircleStop,
+        description: "Categoria 5",
+    },
+    {
+        icon: CircleStop,
+        description: "Categoria 6",
+    },
+    {
+        icon: CircleStop,
+        description: "Categoria 7",
+    },
+    {
+        icon: CircleStop,
+        description: "Categoria 8",
+    },
+    {
+        icon: CircleStop,
+        description: "Categoria 9",
+    },
+    {
+        icon: CircleStop,
+        description: "Categoria 10",
+    },
+]
