2ed0b78d360524f9fb7f59fbc8807c7f49ad7488
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-12 17:07:19 -0300

    [frontend] remove: informações do usuário menu

--- frontend/src/components/molecules/app-sidebar.tsx
+++ frontend/src/components/molecules/app-sidebar.tsx
@@ -1,14 +1,12 @@
 import { NavMain } from "@/components/molecules/nav-main"
-import { NavUser } from "@/components/molecules/nav-user"
 import { TeamLogo } from "./team-logo"
 import {
     Sidebar,
     SidebarContent,
-    SidebarFooter,
     SidebarHeader,
     SidebarRail,
 } from "@/components/atoms/sidebar"
-import { data } from "./sidebar-data"
+import { data } from "../../utils/sidebar-data"
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
     return (
@@ -19,9 +17,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             <SidebarContent>
                 <NavMain items={data.navMain} />
             </SidebarContent>
-            <SidebarFooter>
-                <NavUser user={data.user} />
-            </SidebarFooter>
             <SidebarRail />
         </Sidebar>
     )
