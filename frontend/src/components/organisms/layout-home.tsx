ad989209e44de4d021a997b6d5e107e52d4fcc78
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-12 15:32:22 -0300

    [frontend] fix: aviso do console

--- frontend/src/components/organisms/layout-home.tsx
+++ frontend/src/components/organisms/layout-home.tsx
@@ -11,14 +11,14 @@ export function LayoutHome({ children }: { children: ReactNode }) {
         <SidebarProvider>
             <AppSidebar />
             <SidebarInset className="bg-zinc-50 flex flex-col w-full">
-                <header className="flex h-12 shrink-0 items-center transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+                <header className="flex h-12 shrink-0 items-center fixed transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
                     <div className="flex items-center gap-2 px-4">
                         <SidebarTrigger />
                     </div>
                 </header>
-                <body className="flex flex-1 flex-col gap-4 p-4 pt-0">
+                <div className="flex flex-1 flex-col gap-4 p-4 pt-16">
                     {children}
-                </body>
+                </div>
             </SidebarInset>
         </SidebarProvider>
     )
