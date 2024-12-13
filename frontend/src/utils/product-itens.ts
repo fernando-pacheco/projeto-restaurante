63ace199438ff1ceef671fca81f63dc98fbd2a6b
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-13 17:07:35 -0300

    [frontend] feat: util para listagem filtrada

---
+++ frontend/src/utils/product-itens.ts
@@ -0,0 +1,57 @@
+export const productItens = [
+    {
+        id: "combo1",
+        tag: "promocao",
+        name: "Combo 1",
+        description: "2 Tradiças e 1 batata",
+        price: 40,
+        newPrice: 32,
+    },
+    {
+        id: "combo2",
+        tag: "promocao",
+        name: "Combo 2",
+        description: "2 Salada Burguer e 1 batata",
+        price: 45,
+        newPrice: 40,
+    },
+    {
+        id: "combo3",
+        tag: "promocao",
+        name: "Combo 3",
+        description: "2 Hambúrguer Vegetariano e 1 batata",
+        price: 45,
+        newPrice: 36,
+    },
+    {
+        id: "tradica",
+        tag: "burger",
+        name: "Tradiça",
+        description:
+            "Pão brioche, hamburguer artesanal 180g, maionese e molho de alho",
+        price: 23,
+    },
+    {
+        id: "saladaburguer",
+        tag: "burger",
+        name: "Tradiça",
+        description:
+            "Pão brioche, hamburguer artesanal 180g, maionese e molho de alho",
+        price: 25,
+    },
+    {
+        id: "hamburguervegetariano",
+        tag: "extra",
+        name: "Hamburguer Vegetariano",
+        description:
+            "Pão brioche, hamburguer vegetariano artesanal 180g, maionese e molho de alho",
+        price: 34,
+    },
+    {
+        id: "batata",
+        tag: "entrada",
+        name: "Batata frita",
+        description: "Porção de 200g de batatas fritas",
+        price: 12,
+    },
+]
