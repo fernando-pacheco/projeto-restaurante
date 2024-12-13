8741eea9fd34628fdef9c2733d64936d17be2bea
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-13 13:37:20 -0300

    [frontend] feat: cor de fundo do card de busca

--- frontend/src/components/molecules/card-search.tsx
+++ frontend/src/components/molecules/card-search.tsx
@@ -8,7 +8,7 @@ interface CardSearchProp {
 
 export function CardSearch({ description, icon }: CardSearchProp) {
     return (
-        <Card className="bg-transparent">
+        <Card className="bg-zinc-50">
             <button>
                 <CardContent className="flex flex-col justify-between items-center flex-1 w-36 h-28 p-5">
                     {icon}
