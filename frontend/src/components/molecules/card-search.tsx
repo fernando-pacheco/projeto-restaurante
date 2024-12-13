85b65a8a1bf0bc1bae3bf2cfa4ab894fb98f8ffa
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-13 17:11:09 -0300

    [frontend] feat: estilização condicional para o card de busca

--- frontend/src/components/molecules/card-search.tsx
+++ frontend/src/components/molecules/card-search.tsx
@@ -1,20 +1,63 @@
 import { ReactNode } from "react"
-import { Card, CardContent, CardDescription } from "../atoms/card"
+import { Separator } from "../atoms/separator"
+import { ChevronRight } from "lucide-react"
+import clsx from "clsx"
 
 interface CardSearchProp {
     icon: ReactNode
     description: string
+    variant?: "default" | "selected"
 }
 
-export function CardSearch({ description, icon }: CardSearchProp) {
+export function CardSearch({
+    description,
+    icon,
+    variant = "default",
+}: CardSearchProp) {
+    const isSelected = variant === "selected"
+
     return (
-        <Card className="bg-zinc-50">
-            <button>
-                <CardContent className="flex flex-col justify-between items-center flex-1 w-36 h-28 p-5">
+        <div
+            className={clsx(
+                "flex flex-col justify-between items-center flex-1 w-32 h-52 rounded-xl py-6 border-salmon-500 border",
+                {
+                    "bg-zinc-50": !isSelected,
+                    "bg-salmon-600": isSelected,
+                },
+            )}
+        >
+            <div className="flex flex-col items-center gap-2">
+                <div
+                    className={clsx("p-2", {
+                        "rounded-full bg-salmon-50 ": isSelected,
+                        "": !isSelected,
+                    })}
+                >
                     {icon}
-                    <CardDescription>{description}</CardDescription>
-                </CardContent>
-            </button>
-        </Card>
+                </div>
+                <span
+                    className={clsx("font-semibold", {
+                        "text-salmon-50": isSelected,
+                        "": !isSelected,
+                    })}
+                >
+                    {description}
+                </span>
+            </div>
+            <div className="flex flex-col gap-6 w-1/2 items-center">
+                <Separator
+                    className={clsx("border-2 rounded-full", {
+                        "bg-salmon-600 border-salmon-600": !isSelected,
+                        "bg-salmon-50 border-salmon-50": isSelected,
+                    })}
+                />
+                <ChevronRight
+                    className={clsx("rounded-full size-6 p-0.5", {
+                        "bg-salmon-600 text-salmon-50": !isSelected,
+                        "bg-salmon-50 text-salmon-600": isSelected,
+                    })}
+                />
+            </div>
+        </div>
     )
 }
