bcd1ec511d98a3c46576082251e4c11f2603dc22
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-13 17:09:09 -0300

    [frontend] feat: bordar no cabeçalho do layout padrão

--- frontend/src/components/organisms/layout-home.tsx
+++ frontend/src/components/organisms/layout-home.tsx
@@ -13,7 +13,7 @@ export function LayoutHome({ children }: { children: ReactNode }) {
         <SidebarProvider className="w-full">
             <AppSidebar />
             <SidebarInset className="bg-salmon-50 flex flex-col relative">
-                <header className="flex h-12 shrink-0 bg-zinc-50 py-10 items-center sticky top-0 z-10 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 w-full">
+                <header className="flex h-12 shrink-0 bg-zinc-50 py-10 items-center sticky top-0 z-10 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 w-full border border-b-salmon-300">
                     <div className="flex items-center gap-2 px-4 justify-center flex-1">
                         <SidebarTrigger />
                         <SearchBar />
