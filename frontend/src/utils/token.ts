faf8406cde482ccae0eba97d3fff92dc0d6ccc24
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-16 14:22:10 -0300

    [frontend] feat: timout para remoção do token do cookie

--- frontend/src/utils/token.ts
+++ frontend/src/utils/token.ts
@@ -1,14 +1,31 @@
 import Cookies from "js-cookie"
 
-export function setToken(access_token: string) {
+let tokenTimeout: NodeJS.Timeout | null = null
+
+export function setToken(access_token: string, expiresIn: number) {
     Cookies.set("jwt_token", access_token, {
-        secure: true,
         sameSite: "strict",
+        secure: true,
     })
+
+    if (tokenTimeout) {
+        clearTimeout(tokenTimeout)
+    }
+
+    tokenTimeout = setTimeout(() => {
+        revokeToken()
+        window.location.reload()
+    }, expiresIn * 1000)
 }
 
 export function revokeToken() {
-    Cookies.remove("jwt_token")
+    if (tokenTimeout) {
+        clearTimeout(tokenTimeout)
+    }
+    Cookies.remove("jwt_token", {
+        sameSite: "strict",
+        secure: true,
+    })
 }
 
 export function getToken() {
