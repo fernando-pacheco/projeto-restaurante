13865908c12c5756f634238ec02a39830626bcf5
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-12-20 16:47:35 -0300

    [frontend] fix: renderização do logo

--- frontend/src/utils/sidebar-data.ts
+++ frontend/src/utils/sidebar-data.ts
@@ -11,7 +11,7 @@ import {
 export const data = {
     team: {
         name: "Acme Inc",
-        logo: "static/ks-logo.png",
+        logo: "/static/ks-logo.png",
         plan: "Enterprise",
     },
     navMain: [
